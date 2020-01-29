69b221e31eab8b6e0ae9ac07492f31268ce136b7
Stephen Schau <81457@GEHRHSD.LOCAL>
2020-01-29 11:49:13 -0500

    Update team4.py
    Naming and describing strategy

--- team4.py
+++ team4.py
@@ -7,8 +7,8 @@
 ####
 
 team_name = 'Baby Yoda' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+strategy_name = 'Random Strategy'
+strategy_description = 'Randomly decides c or b'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
